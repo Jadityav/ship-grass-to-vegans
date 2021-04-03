08dee177c802fa060bd36326439fbf4d94b66c4f
jyotir <jadityav@gmail.com>
2021-04-03 21:10:07 +0530

    refactoring

--- build.py
+++ build.py
@@ -1,27 +1,16 @@
-print ('hello')
+import os
 
-top_html = open ('./templates/top.html').read()
-bottom_html = open ('./templates/bottom.html').read()
-middle_html = open ('./content/index.html').read()
+page_names = os.listdir("./content")
+for x in page_names:
 
-combined = top_html  + middle_html + bottom_html
+    path_origin = str("./content/" + x)
+    
+    top_html = open ('./templates/top.html').read()
+    bottom_html = open ('./templates/bottom.html').read()
+    middle_html = open (path_origin).read()
 
-open ('./docs/index.html', 'w+').write(combined)
+    combined = top_html  + middle_html + bottom_html
 
-
-top_html = open ('./templates/top.html').read()
-bottom_html = open ('./templates/bottom.html').read()
-middle_html = open ('./content/order.html').read()
-
-combined = top_html  + middle_html + bottom_html
-
-open ('./docs/order.html', 'w+').write(combined)
-
-
-top_html = open ('./templates/top.html').read()
-bottom_html = open ('./templates/bottom.html').read()
-middle_html = open ('./content/prices.html').read()
-
-combined = top_html  + middle_html + bottom_html
-
-open ('./docs/prices.html', 'w+').write(combined)
+    path_destination= str("./docs/" + x)
+    
+    open (path_destination, 'w+').write(combined)
